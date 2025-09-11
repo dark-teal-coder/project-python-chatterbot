22031228a14ae98d06668808cb79a1035525f8d1
DarkTealCoder <95575665+dark-teal-coder@users.noreply.github.com>
2025-09-11 16:53:16 +0800

    Add bot.py

---
+++ bot.py
@@ -0,0 +1,30 @@
+# bot.py
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from cleaner import clean_corpus
+
+CORPUS_FILE = "training_data.txt"
+
+chatbot = ChatBot("Chatpot")
+
+trainer = ListTrainer(chatbot)
+# trainer.train([
+#     "Hi",
+#     "Welcome, friend 😃",
+# ])
+# trainer.train([
+#     "Are you a plant?",
+#     "No, I'm the pot below the plant!",
+# ])
+cleaned_corpus = clean_corpus(CORPUS_FILE)
+trainer.train(cleaned_corpus)
+
+exit_conditions = (":q", "quit", "exit")
+
+while True:
+    query = input(">>> ")
+    if query in exit_conditions:
+        break
+    else:
+        print(f"🌱 {chatbot.get_response(query)}")
