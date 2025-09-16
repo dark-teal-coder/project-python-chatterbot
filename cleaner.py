ede80cf8a6c1cbf7b3047abe40062f6c731e4fee
DarkTealCoder <95575665+dark-teal-coder@users.noreply.github.com>
2025-09-16 16:49:39 +0800

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -20,7 +20,7 @@ def remove_chat_metadata(chat_export_file):
 def remove_non_message_text(export_text_lines):
     messages = export_text_lines[1:-1]
     filter_out_msgs = ("<Media omitted>",)
-    return tuple((msg for msg in messages if msg not in filter_out_msgs))
+    return tuple([msg for msg in messages if msg not in filter_out_msgs])
 
 # if __name__ == "__main__":
 #     message_corpus = remove_chat_metadata("training_data.txt")
@@ -31,3 +31,4 @@ def clean_corpus(chat_export_file):
     message_corpus = remove_chat_metadata(chat_export_file)
     cleaned_corpus = remove_non_message_text(message_corpus)
     return cleaned_corpus
+
